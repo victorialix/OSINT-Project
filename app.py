575535f076afcfa9186163459f76f8b868ad25fb
rmaldonadogutierrez <rmaldonadogutierrez@hawk.illinoistech.edu>
2026-04-25 22:48:39 -0500

    Update app.py with final flask wiring

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.travel_routes import travel_bp
 from routes.chart_routes import chart_bp
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    # Register blueprints
-    app.register_blueprint(travel_bp, url_prefix="/api")
-    app.register_blueprint(chart_bp, url_prefix="/api")
+# Register blueprints
+app.register_blueprint(travel_bp, url_prefix="/api")
+app.register_blueprint(chart_bp, url_prefix="/api")
 
-    return app
+# Home page
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
